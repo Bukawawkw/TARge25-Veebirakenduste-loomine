8fd5e4b43afd6d9df43ca1888a2008389dab6b6e
Bukawa <Bukawawkw@users.noreply.github.com>
2026-05-06 13:25:06 +0300

    [FullStackReact]: PlanetsController base implementation

---
+++ 07_FullStackReact/07_FullStackReact.Server/Controllers/PlanetsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _07_FullStackReact.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlanetsController : ControllerBase
+    {
+        public IActionResult SchoolIndex()
+        {
+            return Ok();
+        }
+    }
+}
