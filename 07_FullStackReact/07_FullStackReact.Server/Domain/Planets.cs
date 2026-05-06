a911cacc656d13fd5abab1f7c7e8ea45c0ae4884
Bukawa <Bukawawkw@users.noreply.github.com>
2026-05-06 13:29:36 +0300

    [FullStackReact]: Small Planets.cs formatting fix

--- 07_FullStackReact/07_FullStackReact.Server/Domain/Planets.cs
+++ 07_FullStackReact/07_FullStackReact.Server/Domain/Planets.cs
@@ -4,7 +4,7 @@
     {
         public Guid PlanetsId { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string Description {  get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
         public int Mass { get; set; }
     }
